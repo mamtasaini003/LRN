2cea4062a0f0286786c977932cebba21439243ae
mamtasaini003 <myselfdk03@gmail.com>
2025-12-31 09:22:20 +0530

    (fix): remove synthetic dataset.

--- src/data/__init__.py
+++
@@ -1,3 +0,0 @@
-from .pde_datasets import BurgersDataset, Burgers2dDataset, DarcyDataset, NavierStokesDataset, create_dataloaders
-
-__all__ = ["BurgersDataset", "Burgers2dDataset", "DarcyDataset", "NavierStokesDataset", "create_dataloaders"]
